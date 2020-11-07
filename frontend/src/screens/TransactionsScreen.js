6844af4312771ad2f66e2502fe6581d08dcf539d
JonathanWang123 <jonathan.wang830@gmail.com>
2020-11-07 16:39:14 -0500

    Added Account Settings

---
+++ frontend/src/screens/TransactionsScreen.js
@@ -0,0 +1,50 @@
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
+import data from '../data';
+import '../styles/Account.css'
+
+function TransactionsScreen(props){
+    const product = data.products.find(x=>x._id === props.match.params.id);
+    const [currentImage, setImage] = useState(0);
+    const changeImage = (change)=>{
+        var newImage = currentImage + change;
+        if(newImage >= product.images.length)
+            newImage = 0;
+        else if (newImage < 0)
+            newImage = product.images.length - 1;
+        setImage(newImage)
+    }
+
+    return( 
+    <div>
+        <div id="AccountOptions" className = "sidenav">
+            <Link to="/AccountSettings"><div className="navItem"><settingStatus/>&nbsp;&nbsp;Personal Information</div></Link>
+            <Link to="/SecuritySettings"><div className="navItem"><settingStatus/>&nbsp;&nbsp;Login & Security</div></Link>
+            <Link to="/TransactionsSettings"><div className="navItem"><settingActive/>&nbsp;&nbsp;Your Transactions</div></Link>
+            <Link to="/ListingsSettings"><div className="navItem"><settingStatus/>&nbsp;&nbsp;Your Listings</div></Link>
+        </div>
+        <div className = "settings">
+            <div className = "headText"><h1>Your Transactions<hr/></h1></div>
+            <ul className="products">
+                {data.products.map(product=>
+                <li>
+                    <div className="product">
+                        <Link to={"/products/"+product._id}>
+                            <img className="product-image" src={product.images[0]} alt="product"/>
+                        </Link>
+                        <div className="product-name">
+                            <Link to={"/products/"+product._id}>
+                                {product.name}
+                            </Link>
+                        </div>
+                        <div className="product-price">${product.price}</div>
+                    </div>
+                </li>
+                )}
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default TransactionsScreen;
