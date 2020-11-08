9605463b68b0d17468546221647b96ca84750601
Jeffrey Yang <jeffreyyang217@gmail.com>
2020-11-08 01:12:05 -0500

    fixed login and register responses

--- backend/marketplace.py
+++ backend/marketplace.py
@@ -1,6 +1,6 @@
-#add
-#delete
-#prices, user, item name, image array
+# add
+# delete
+# prices, user, item name, image array
 import pymongo
 import json
 import textdistance as td
@@ -10,26 +10,28 @@ from pymongo import MongoClient
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-#Create instance of Flask App
+# Create instance of Flask App
 app = Flask(__name__)
 CORS(app)
 
-#Connects to Mongo Database
+# Connects to Mongo Database
 connection_url = 'mongodb+srv://ruteam:ruscrew@cluster0.bvss2.mongodb.net/marketplace?retryWrites=true&w=majority'
 client = pymongo.MongoClient(connection_url)
 
-#clean up and standardize
+# clean up and standardize
+
+
 def textFormat(text):
     newtext = ""
     for a in text:
         if a.isalpha() == True or a.isspace() == True:
-            newtext+=a
+            newtext += a
     newtext.lower()
     newtext = list(newtext.split(" "))
-    #print(newtext)
+    # print(newtext)
     return newtext
-    
-#Search items in database - basic search
+
+# Search items in database - basic search
 @app.route('/search', methods=['GET'])
 def search():
     db = client['marketplace']
@@ -42,25 +44,26 @@ def search():
 
     search_term = search_info['search_term'].lower()
     search_t = textFormat(search_term)
-    #match terms in tags,title and description
+    # match terms in tags,title and description
     for tags in search_t:
-        search_tags = items.find({'tags': {'$elemMatch': { '$eq' : tags } } })
+        search_tags = items.find({'tags': {'$elemMatch': {'$eq': tags}}})
         search_items.extend(list(search_tags))
-    
-    search_title = items.find({'title': {'$regex': ".*" + search_term + ".*"  } })
-    search_desc = items.find({'description': {'$regex': ".*" + search_term + ".*" } })
-    
+
+    search_title = items.find({'title': {'$regex': ".*" + search_term + ".*"}})
+    search_desc = items.find(
+        {'description': {'$regex': ".*" + search_term + ".*"}})
+
     search_items.extend(list(search_title))
     search_items.extend(list(search_desc))
-    #string similarity- leveshtein algorithm for scanning title and description
+    # string similarity- leveshtein algorithm for scanning title and description
     for query in items.find():
         try:
             desc = query["description"]
             title = query["title"]
-            
+
             title_t = textFormat(title)
             desc_t = textFormat(desc)
-            
+
             flag = 0
             for input_1 in search_t:
                 for input_2 in title_t:
@@ -73,7 +76,7 @@ def search():
                         break
                 if flag == 1:
                     break
-                
+
                 for input_3 in desc_t:
                     #print(td.levenshtein.normalized_similarity(input_1, input_3))
                     if td.levenshtein.normalized_similarity(input_1, input_3) > .5:
@@ -88,12 +91,12 @@ def search():
             pass
     jsondata = dumps(search_items)
     jsonlist = json.loads(jsondata)
-    #remove duplicate objects
-    jsons = { repr(each): each for each in jsonlist }.values()
+    # remove duplicate objects
+    jsons = {repr(each): each for each in jsonlist}.values()
 
     return dumps(jsons)
 
-#Deletes Item from items database
+# Deletes Item from items database
 @app.route('/deleteItem', methods=['POST'])
 def delete_item():
     db = client['marketplace']
@@ -105,7 +108,7 @@ def delete_item():
         items.delete_one(x)
     return "success"
 
-#Adds Item to items database
+# Adds Item to items database
 @app.route('/addItem', methods=['POST'])
 def add_item():
     db = client['marketplace']
@@ -113,8 +116,8 @@ def add_item():
     new_item = request.get_json()
     items.insert_one(new_item)
     return "success"
-    
-#Edit Items in database
+
+# Edit Items in database
 @app.route('/editItem', methods=['POST'])
 def edit_item():
     db = client['marketplace']
@@ -127,31 +130,32 @@ def edit_item():
     items.insert_one(new_item)
     return "success"
 
-#Adds new user to database
-@app.route('/addUser', methods=['POST'])
+# Adds new user to database
+@app.route('/register', methods=['POST'])
 def add_user():
     db = client['marketplace']
     users = db['users']
     new_user = request.get_json()
     for document in users.find({}, projection={"_id": False}):
-        if document["netid"] == new_user['netid']:
-            return json.dumps({"msg": "exists"})
+        if document["email"] == new_user['email']:
+            return json.dumps({"success": False, "msg": "An account exists for this email"})
     users.insert_one(new_user)
-    return json.dumps({"msg": "created"})
+    return json.dumps({"success": True, "msg": "Account successfully created", "firstName": document["firstName"], "lastName": document["lastName"], "email": document["email"]})
 
-#Checks login credentials
+# Checks login credentials
 @app.route('/login', methods=['POST'])
 def login():
     db = client['marketplace']
     users = db['users']
     attempt = request.get_json()
-    for document in users.find({}, projection = {"_id" : False}):
-        if document["netid"] == attempt['netid']:
+    for document in users.find({}, projection={"_id": False}):
+        if document["email"] == attempt['email']:
             if document["password"] == attempt['password']:
-                return json.dumps({"msg": "correct"} )
+                return json.dumps({"success": True, "msg": "Login successful", "firstName": document["firstName"], "lastName": document["lastName"], "email": document["email"]})
             else:
-                return json.dumps({"msg": "incorrect information"})
-    return json.dumps({"msg": "user not found"})
+                return json.dumps({"success": False, "msg": "Incorrect password"})
+    return json.dumps({"success": False, "msg": "No account exists for this email"})
+
 
 if __name__ == "__main__":
     app.run()
