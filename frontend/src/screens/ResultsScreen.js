3d957044c5686319a9000374134a3101a6a50745
JonathanWang123 <jonathan.wang830@gmail.com>
2020-11-08 12:43:25 -0500

    Added Other Items

--- frontend/src/screens/ResultsScreen.js
+++ frontend/src/screens/ResultsScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import data from '../data';
+import biotextbook from "../images/biotextbook.jpg";
+import calctextbook from "../images/calctextbook.jpg";
 
 function ResultsScreen(props){
     var results;
     function doSearch(){
         let searchTerm = document.getElementById("searchBar").value;
-        document.getElementById('searchTerm').innerHTML = (searchTerm);
         var request = new XMLHttpRequest();
         request.open("GET", "http://127.0.0.1:5000/search?searchTerm=" + searchTerm);
         request.onload = function() {
@@ -18,8 +19,34 @@ function ResultsScreen(props){
     }
     return <div className="homeContainer" onLoad={doSearch}>
         <div className="homeCategory" id="homeTextbooks">
-            <div className="homeHeader">Search Results for <div id="searchTerm" style={{color:'#901818',display: 'inline-block'}}></div><hr/></div>
+            <div className="homeHeader">Search Results for textbook<hr/></div>
             <ul className="products">
+                <li>
+                    <div className="product">
+                        <Link to="/listings/5fa7f43aa22b740eaba2a950">
+                            <img className="product-image" src={biotextbook} alt="product"/>
+                        </Link>
+                        <div className="product-name">
+                            <Link to="/listings/5fa7f43aa22b740eaba2a950">
+                                Campbell Biology
+                            </Link>
+                        </div>
+                        <div className="product-price">$120</div>
+                    </div>
+                </li>
+                <li>
+                <div className="product">
+                        <Link to="">
+                            <img className="product-image" src={calctextbook} alt="product"/>
+                        </Link>
+                        <div className="product-name">
+                            <Link to="">
+                                Calculus Textbook
+                            </Link>
+                        </div>
+                        <div className="product-price">$100</div>
+                    </div>
+                </li>
                 {data.products.map(product=>
                 <li>
                     <div className="product">
